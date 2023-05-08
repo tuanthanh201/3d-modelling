71ebecad710840d27721318cbad5ecc3eec98adb
Thanh Nguyen <ngthanh2248@gmail.com>
2023-05-07 20:20:23 -0700

    Add components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,15 @@
+import Customizer from './pages/Customizer';
+import Home from './pages/Home';
+import Canvas from './canvas';
+
 const App = () => {
-  return <h1>Hello world</h1>;
+  return (
+    <main className="app transition-all ease-in">
+      <Home />
+      <Canvas />
+      <Customizer />
+    </main>
+  );
 };
 
 export default App;
