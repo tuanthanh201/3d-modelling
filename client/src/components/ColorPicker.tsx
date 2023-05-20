1bafe29dfd755367289cd764d529b8f802550613
Thanh Nguyen <ngthanh2248@gmail.com>
2023-05-20 13:24:28 -0700

    Implement ColorPicker

--- client/src/components/ColorPicker.tsx
+++ client/src/components/ColorPicker.tsx
@@ -1,5 +1,20 @@
+import { SketchPicker } from 'react-color';
+import { useSnapshot } from 'valtio';
+
+import state from '../store';
+
 const ColorPicker = () => {
-  return <h1>Color Picker</h1>;
+  const snap = useSnapshot(state);
+
+  return (
+    <div className="absolute left-full ml-3">
+      <SketchPicker
+        color={snap.color}
+        disableAlpha
+        onChange={(color) => (state.color = color.hex)}
+      />
+    </div>
+  );
 };
 
 export default ColorPicker;
