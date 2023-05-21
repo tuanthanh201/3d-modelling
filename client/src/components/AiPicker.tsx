0676e172f687fe50b395a7fd8f7531d653c6636e
Thanh Nguyen <ngthanh2248@gmail.com>
2023-05-20 17:51:53 -0700

    Implement AiPicker

--- client/src/components/AiPicker.tsx
+++ client/src/components/AiPicker.tsx
@@ -1,5 +1,7 @@
 import { DecalType } from '../config/constants';
 
+import { CustomButton } from '.';
+
 interface AiPickerProps {
   prompt: string;
   setPrompt: (prompt: string) => void;
@@ -13,7 +15,41 @@ const AiPicker = ({
   generatingImg,
   handleSubmit,
 }: AiPickerProps) => {
-  return <h1>Ai Picker</h1>;
+  return (
+    <div className="aipicker-container">
+      <textarea
+        className="aipicker-textarea"
+        placeholder="Ask AI..."
+        rows={5}
+        value={prompt}
+        onChange={(e) => setPrompt(e.target.value)}
+      />
+
+      <div className="flex flex-wrap gap-3">
+        {generatingImg ? (
+          <CustomButton
+            type="outline"
+            title="Asking AI..."
+            customStyle="text-xs"
+            disabled
+          />
+        ) : (
+          <>
+            <CustomButton
+              type="outline"
+              title="AI Logo"
+              handleClick={() => handleSubmit('logo')}
+            />
+            <CustomButton
+              type="filled"
+              title="AI Full"
+              handleClick={() => handleSubmit('full')}
+            />
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default AiPicker;
